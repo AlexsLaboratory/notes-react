fe89684131d974d657bf1100932e28b8ae542f86
Alex Lowe <loweman64@outlook.com>
2022-07-29 22:17:58 -0400

    fix: Input Error Message Style Module
    Add module syntax for input error messages.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -20,7 +20,7 @@ const Input: FunctionComponent<Props> = (props) => {
         <div className={formStyles["form-group"]}>
             <label className={formStyles["form-group__label"]} htmlFor={props.name}>{props.label}</label>
             <input className={formStyles["form-group__input"]} type={props.type} name={props.name} id={props.name} onChange={props.onChange} onBlur={props.onBlur} value={props.value}/>
-            {props.hasError && <span className="form-group__error">{props.errorMessage}</span>}
+            {props.hasError && <span className={`${formStyles["form-group__error"]}`}>{props.errorMessage}</span>}
         </div>
     );
 };
