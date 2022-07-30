758804a6aefac31b6b9b14996a7e79568eed26a7
Alex Lowe <loweman64@outlook.com>
2022-07-29 21:14:36 -0400

    fix: Allow Styles to Override Others
    Override the grid class and remove width: 100% on Input component.

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,12 +1,11 @@
 import React, {FunctionComponent} from 'react';
 import AuthProvider from "../providers/AuthProvider";
 import Header from "../components/Header";
-import "../scss/components/_forms.scss";
-import "../scss/pages/_create_view_edit.scss";
 import Form from "../components/Form";
 import Button from '../components/Button';
 import {useInput, ErrorMessage} from "../hooks/useInput";
 import Input from "../components/Input";
+import formStyles from '../scss/modules/form.module.scss';
 
 interface OwnProps {
 }
@@ -28,22 +27,22 @@ const Signup: FunctionComponent<Props> = (props) => {
     return (
         <AuthProvider>
             <Header/>
-            <div className="grid grid--site-form site-form">
-                <h1 className="site-form__title">Signup</h1>
-                <div className="site-form__content site-form__content--create-edit">
+            <div className={`grid ${formStyles["grid--site-form"]} ${formStyles["site-form"]}`}>
+                <h1 className={`${formStyles["site-form__title"]}`}>Signup</h1>
+                <div className={`${formStyles["site-form__content"]} ${formStyles["site-form__content--create-edit"]}`}>
                     <Form className="form" onSubmit={(e) => {
                         e.preventDefault();
                         console.log(e);
                         reset();
                     }}>
-                        <div className="form-fields">
+                        <div className={`${formStyles["form-fields"]}`}>
                             <Input label="First Name" name="fname" type="text" />
                             <Input label="Last Name" name="lname" type="text" />
                             <Input label="Email" name="email" type="email" value={emailValue} onChange={valueChangeHandler} onBlur={inputBlurHandler} hasError={hasError} errorMessage={errorMessage} />
                             <Input label="Password" name="current-password" type="password" />
                         </div>
                         <Button type="submit"
-                                className="button button--primary button--nav site-form__action-btn site-form__action-btn--save form__button" styleType="primary">
+                                className={`${formStyles["site-form__action-btn"]} ${formStyles["site-form__action-btn--save"]} ${formStyles.form__button}`} styleType="primary">
                             Create
                         </Button>
                     </Form>
