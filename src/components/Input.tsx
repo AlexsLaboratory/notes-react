f78499e9e3358dfc2191a117f3418aa079077aa7
Alex Lowe <loweman64@outlook.com>
2022-08-08 12:55:47 -0400

    feat: Add Input Error Color to Field

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -19,7 +19,7 @@ const Input: FunctionComponent<Props> = (props) => {
     return (
         <div className={inputStyles["input-group"]}>
             <label className={inputStyles["input-group__label"]} htmlFor={props.name}>{props.label}</label>
-            <input className={inputStyles["input-group__input"]} type={props.type} name={props.name} id={props.name} onChange={props.onChange} onBlur={props.onBlur} value={props.value}/>
+            <input className={`${inputStyles["input-group__input"]} ${props.hasError ? `${inputStyles["input-group__input--error"]}` : ""}`} type={props.type} name={props.name} id={props.name} onChange={props.onChange} onBlur={props.onBlur} value={props.value}/>
             {props.hasError && <span className={`${inputStyles["input-group__error"]}`}>{props.errorMessage}</span>}
         </div>
     );
