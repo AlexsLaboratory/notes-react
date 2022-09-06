7db1aa7464bb67c61e9c7decd41b405f049019b5
Alex Lowe <loweman64@outlook.com>
2022-09-06 14:07:24 -0400

    feat: Convert Timestamp to Elapsed Time

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -5,6 +5,7 @@ import headerStyles from "../scss/modules/header.module.scss";
 import ButtonLink from "../components/ButtonLink";
 import Button from "../components/Button";
 import Note from "../components/Note";
+import {timeSince} from "../utils/time";
 
 function App() {
     return (
@@ -13,7 +14,7 @@ function App() {
             <div className={`${homeStyles.grid} ${homeStyles['grid--content']} ${homeStyles.content}`}>
                 <h1 className={`${homeStyles['content__title']}`}>My Notes</h1>
                 <div className={`${homeStyles['grid']} ${homeStyles['content__items']} ${homeStyles['grid--items']}`}>
-                    <Note title="Hello World" body="lorem afkljafljalfjsalkfj aksdjfla fjlkas fjsalf lsajdl" id="1" timestamp="2 hours ago"/>
+                    <Note title="Hello World" body="lorem afkljafljalfjsalkfj aksdjfla fjlkas fjsalf lsajdl" id="1" timestamp={timeSince("2022-09-06 01:28:42.370000 +00:00")}/>
                 </div>
             </div>
         </>
